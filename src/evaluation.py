325febf140eb92000a8c5b4e99d4b772f17ba743
Lukáš Rynt <ryntluka@fit.cvut.cz>
2022-10-16 22:10:12 +0200

    Add relevance metric

--- src/evaluation.py
+++ src/evaluation.py
@@ -15,6 +15,7 @@ class Evaluator:
         success_1 = 0
         success_5 = 0
         success_10 = 0
+        relevance = 0
         for i in range(len(en_words)):
             most_similar = self.__get_most_similar_for(en_words[i], 'cz', top_k=10)
             if cz_words[i] in most_similar['token'].unique():
@@ -23,10 +24,31 @@ class Evaluator:
                 success_5 += 1
             if cz_words[i] in most_similar['token'].head(1).unique():
                 success_1 += 1
+            relevance += self.__get_relevance_score(most_similar, cz_words[i])
 
-        print("P@1: ", success_1 / len(en_words))
-        print("P@5: ", success_5 / len(en_words))
-        print("P@10:", success_10 / len(en_words))
+        print("P@1: {:.2f}%".format(success_1 / len(en_words) * 100))
+        print("P@5: {:.2f}%".format(success_5 / len(en_words) * 100))
+        print("P@10: {:.2f}%".format(success_10 / len(en_words) * 100))
+        print("Relevance: {:.2f}%".format(relevance / len(en_words) * 100))
+
+    def relevance_metric(self, en_words: List[str], cz_words: List[str], max_k: int = 10) -> float:
+        """
+        Metric that takes max_k words as translations for each word in en_words. Then it takes which of these
+        translations were contained the correct translation. If none of them did, it assigns zero, otherwise it
+        contributes toward the final metric as 1/l, where l-th word was the correct translation. In the end it
+        is normalized by the len of word pairs
+        :param en_words:
+        :param cz_words:
+        :param max_k:
+        :return:
+        """
+        en_words = self.__prefix_words(en_words, 'en')
+        cz_words = self.__prefix_words(cz_words, 'cz')
+        total_score = 0
+        for i in range(len(en_words)):
+            most_similar = self.__get_most_similar_for(en_words[i], 'cz', top_k=max_k)
+            total_score += self.__get_relevance_score(most_similar, cz_words[i])
+        return total_score / len(en_words)
 
     def p_at_k_metric(self, en_words: List[str], cz_words: List[str], k: int = 5) -> float:
         """
@@ -47,6 +69,20 @@ class Evaluator:
             if cz_words[i] in most_similar['token'].unique():
                 success_count += 1
         return success_count / len(en_words)
+
+    def __get_relevance_score(self, most_similar: pd.DataFrame, translation: str) -> float:
+        l_val = self.__find_in_list(most_similar['token'].tolist(), translation)
+        if l_val == -1:
+            return 0
+        else:
+            return 1 / (l_val + 1)
+
+    @staticmethod
+    def __find_in_list(lst: List, seeked):
+        for i in range(len(lst)):
+            if lst[i] == seeked:
+                return i
+        return -1
 
     def __get_most_similar_for(self, word: str, language: str, top_k: int) -> pd.DataFrame:
         """
